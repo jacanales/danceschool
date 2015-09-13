ec98f6947e7126880c7432258ab69e22a069d2aa
Jesús A. Canales <jacanalesdiez@gmail.com>
2015-09-13 19:57:17 +0200

    Added grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,44 @@
-module.exports = function(grunt) {
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Show elapsed time at the end
+    require('time-grunt')(grunt);
+
+    // Load all grunt tasks
+    require('load-grunt-tasks')(grunt);
+    grunt.loadTasks('tasks');
+
+    var directoriesConfig = {
+        composer: 'vendor',
+        composerBin: 'vendor/bin',
+        reports: 'logs',
+        php: 'app'
+    }
 
     // Project configuration.
     grunt.initConfig({
+        directories: directoriesConfig,
         pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc',
+                reporter: require('jshint-stylish')
+            },
+            all: [
+                'Gruntfile.js'
+            ]
+        },
+        jsvalidate: {
+            files: [
+                'Gruntfile.js'
+            ]
+        },
+        jsonlint: {
+            files: [
+                '*.json'
+            ]
+        },
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -17,5 +53,13 @@ module.exports = function(grunt) {
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['uglify']);
+    grunt.registerTask('default', ['test']);
+
+    grunt.registerTask('test', [
+        'jsvalidate',
+        'jshint',
+        'jsonlint'
+    ]);
+
+    grunt.registerTask('uglify', ['uglify']);
 }
