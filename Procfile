b82d2d2072f3dbef9b1008e0033d51675dbe0a2a
Jesús A. Canales <jacanalesdiez@gmail.com>
2015-09-13 15:08:48 +0200

    Process assets on deploy to Heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: bin/heroku-php-apache2 web/
+web: sh ./scripts/install.sh
---
+++ scripts/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+php ../app/console
+php ../app/console cache:clear --env=prod
+php ../app/console assets:install --env=prod
+php ../app/console assetic:dump --env=prod
+php ../app/console doctrine:schema:update --dump-sql
+sh ../bin/run
