2f6f5e728bdae8f28e01f14ab1b30e044fbf35ec
Jesús A. Canales <jacanalesdiez@gmail.com>
2017-03-12 10:42:56 +0100

    First step to upgrade to Symfony 3. Vendors + dependencies

--- web/app.php
+++ web/app.php
@@ -3,7 +3,11 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+/**
+ * @var \Composer\Autoload\ClassLoader $loader
+ */
+$loader = require __DIR__.'/../app/autoload.php';
+include_once __DIR__.'/../var/bootstrap.php.cache';
 
 // Enable APC for autoloading to improve performance.
 // You should change the ApcClassLoader first argument to a unique prefix
@@ -12,9 +16,6 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
 $loader->unregister();
 $apcLoader->register(true);
-
-require_once __DIR__.'/../app/AppKernel.php';
-require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
