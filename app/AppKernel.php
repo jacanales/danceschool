70127e839e8f30a1d7a47fe03081b01f560e4806
Jesús A. Canales <jacanalesdiez@gmail.com>
2017-12-23 13:54:00 +0100

    Move registered bundles to bundles.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,6 +1,5 @@
 <?php
 
-use Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle;
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
@@ -8,52 +7,12 @@ class AppKernel extends Kernel
 {
     public function registerBundles()
     {
-        $bundles = array(
-            # Framework
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle(),
-            new AppBundle\AppBundle(),
-
-            // Bundles the SonataAdminBundle relies on
-            new Sonata\CoreBundle\SonataCoreBundle(),
-            new Sonata\BlockBundle\SonataBlockBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-
-            // The storage and SonataAdminBundle
-            new Sonata\AdminBundle\SonataAdminBundle(),
-            new SonataDoctrineORMAdminBundle(),
-
-            # Third-party bundles
-            new FOS\UserBundle\FOSUserBundle(),
-            new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
-            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
-            new EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle(),
-            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
-//            new Liip\ThemeBundle\LiipThemeBundle(),
-            new Misd\PhoneNumberBundle\MisdPhoneNumberBundle(),
-
-
-        );
-
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-
-            # Third-party bundles
-            $bundles[] = new Webfactory\Bundle\ExceptionsBundle\WebfactoryExceptionsBundle();
-            $bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-//            $bundles[] = new Egulias\ListenersDebugCommandBundle\EguliasListenersDebugCommandBundle();
+        $contents = require $this->getProjectDir() . '/app/config/bundles.php';
+        foreach ($contents as $class => $envs) {
+            if (isset($envs['all']) || isset($envs[$this->environment])) {
+                yield new $class();
+            }
         }
-
-        return $bundles;
     }
 
     /**
