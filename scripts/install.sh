2c31eb6c016a339969d45aa82adb40b36e8f8e1f
Jesús A. Canales <jacanalesdiez@gmail.com>
2017-12-23 13:19:34 +0100

    Remove unused files

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,4 +6,3 @@ php ../app/console cache:clear --env=prod
 php ../app/console assets:install --env=prod
 php ../app/console assetic:dump --env=prod
 php ../app/console doctrine:schema:update --dump-sql
-sh ../bin/run
