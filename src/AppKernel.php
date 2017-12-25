d685e06204aca9c2a1fc96d62706e45503e3e6f9
Jesús A. Canales <jacanalesdiez@gmail.com>
2017-12-25 23:34:21 +0100

    Reorganize config

--- src/AppKernel.php
+++ src/AppKernel.php
@@ -1,22 +1,8 @@
 <?php
 
 use App\Kernel as BaseKernel;
-use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends BaseKernel
 {
-    /**
-     * @param LoaderInterface $loader
-     * @throws Exception
-     */
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $envFolder = $this->getProjectDir() . '/config/' . $this->getEnvironment() . '/config.yaml';
 
-        if (file_exists($envFolder)) {
-            $loader->load($envFolder);
-        } else {
-            $loader->load($this->getProjectDir() . '/config/common/config.yaml');
-        }
-    }
 }
