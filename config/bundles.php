f7857091d1269c8221816754d8fad1f136deafa7
Jesús A. Canales <jacanalesdiez@gmail.com>
2017-12-25 13:00:59 +0100

    Update oauth bundle

--- config/bundles.php
+++ config/bundles.php
@@ -23,6 +23,7 @@ return [
     # Third-party bundles
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
