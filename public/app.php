e8d2659cfe82c293ab6c5fb3d0d9e96d76519af7
Jesús A. Canales <jacanalesdiez@gmail.com>
2017-12-26 13:15:04 +0100

    Move assets and add node Docker image

--- public/app.php
+++ public/app.php
@@ -5,7 +5,7 @@ use Symfony\Component\HttpFoundation\Request;
 /**
  * @var \Composer\Autoload\ClassLoader $loader
  */
-$loader = require __DIR__.'/../app/autoload.php';
+$loader = require __DIR__.'/../src/autoload.php';
 
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
