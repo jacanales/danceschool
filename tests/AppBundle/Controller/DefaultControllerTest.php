047dd88818163f901f7720c357a744db066baea1
Jesús A. Canales <jacanalesdiez@gmail.com>
2017-12-28 15:42:22 +0100

    Fi unitary tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -1,21 +1,24 @@
 <?php
 
-namespace Tests\AppBundle\Controller;
+namespace Tests\App\Controller;
 
+use App\Controller\DefaultController;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * @see DefaultController
+ */
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        $this->markTestSkipped();
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
 
         $response = $client->getResponse();
 
-        $this->assertEquals(200, $response->getStatusCode(), $response->getContent());
-        $this->assertTrue($crawler->filter('html:contains("Homepage")')->count() > 0);
+        $this->assertEquals(302, $response->getStatusCode(), $response->getContent());
+        $this->assertTrue($crawler->filter('html:contains("Redirecting to http://localhost/login")')->count() > 0);
     }
 }
