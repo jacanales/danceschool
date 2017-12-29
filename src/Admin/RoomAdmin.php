3fa07b2f30b8f3860e5eec9eb3a2f9e648311224
Jesús A. Canales <jacanalesdiez@gmail.com>
2017-12-30 00:46:03 +0100

    Fix docker sites and add RoomAdmin

--- src/Admin/RoomAdmin.php
+++ src/Admin/RoomAdmin.php
@@ -7,26 +7,33 @@ use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
-class CourseAdmin extends AbstractAdmin
+class RoomAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper->add('id');
+        $formMapper->add('name');
         $formMapper->add('description');
         $formMapper->add('price');
+        $formMapper->add('address');
+        $formMapper->add('city');
+        $formMapper->add('postalCode');
+        $formMapper->add('phone');
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
     {
         $datagridMapper->add('id');
-        $datagridMapper->add('description');
         $datagridMapper->add('price');
+        $datagridMapper->add('city');
     }
 
     protected function configureListFields(ListMapper $listMapper)
     {
         $listMapper->addIdentifier('id');
+        $listMapper->add('name');
         $listMapper->add('description');
         $listMapper->add('price');
+        $listMapper->add('city');
     }
 }
