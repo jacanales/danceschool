b8bddd3e147463d708d4a033d3d01af065986eb8
Jesús A. Canales <jacanalesdiez@gmail.com>
2018-01-01 21:21:15 +0100

    Work on TeacherAdmin

---
+++ src/Admin/TeacherAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Admin;
+
+
+use App\Form\Type\TeacherType;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class TeacherAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form->add('Teacher', TeacherType::class);
+    }
+
+    protected function configureListFields(ListMapper $list)
+    {
+        $list->addIdentifier('full_name');
+        $list->add('user.email');
+        $list->add('user.city');
+    }
+}
