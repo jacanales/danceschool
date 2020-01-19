c5309e6829e947b3ab60e0b1d6eec0eb1b34b8c5
Jesús Antonio Canales Diez <jacanalesdiez@gmail.com>
2020-01-19 02:07:47 +0100

    Cleanup webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -11,15 +11,6 @@ Encore
     .addEntry('app', './assets/js/app.js')
     .addEntry('login', './assets/js/login.js')
 
-    .addLoader({
-        test: /\.font\.js/,
-        loader: 'webfonts-loader',
-        options: {
-            publicPath: '/fonts/'
-            
-        }
-    })
-
     // will copy assets into public folder
     /*.copyFiles({
         from: './assets/login',
