ea3b2c0bbfa981370b05d6cd7115060fa789675d
Jesús Antonio Canales Diez <jacanalesdiez@gmail.com>
2020-01-19 19:31:20 +0100

    Fix index.php

--- public/index.php
+++ public/index.php
@@ -14,13 +14,13 @@ if ($_SERVER['APP_DEBUG']) {
     Debug::enable();
 }
 
-$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'];
-if ($trustedProxies ?? false) {
+$trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
+if ($trustedProxies) {
     Request::setTrustedProxies(\explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST);
 }
 
-$trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'];
-if ($trustedHosts ?? false) {
+$trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false;
+if ($trustedHosts) {
     Request::setTrustedHosts([$trustedHosts]);
 }
 
