bdc047f699a622092351680d52523d1799807e2c
Jesús Antonio Canales Diez <jacanalesdiez@gmail.com>
2020-11-03 00:31:42 +0100

    fix: npm dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 var Encore = require('@symfony/webpack-encore');
 
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
 Encore
-// the project directory where all compiled assets will be stored
+    // the project directory where all compiled assets will be stored
     .setOutputPath('public/build/')
 
     // the public path used by the web server to access the previous directory
